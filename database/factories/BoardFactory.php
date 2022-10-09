97d3842f32b7dcb66901edbe904600b864f8a4d4
Cheng Zhen Yee <chengzhenyee@gmail.com>
2022-10-09 16:21:46 +0800

    Add factory for Board model

---
+++ database/factories/BoardFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Board;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Board>
+ */
+class BoardFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Board::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->company(),
+            'description' => $this->faker->realText(),
+            'owner_id' => User::factory(),
+        ];
+    }
+}
